8a832958ba595dd8500e061e58549c601ceaf7b8
itsnotLonee <david.balboa.rojo@gmail.com>
2019-10-03 12:46:46 +0200

    commit preparacion

--- js/rgb.js
+++ js/rgb.js
@@ -2,17 +2,24 @@
 
 //var convert = document.getElementById("boton1").addEventListener("click", prueba());
 
-function prueba() {
-  alert("hola");
+function rgb() { 
+
+  var r = document.getElementById("r").value;
+  var g = document.getElementById("g").value;
+  var b = document.getElementById("b").value;
+
+  r = parseInt(r);
+  g = parseInt(g);
+  b = parseInt(b);
+  //alert(r+g+b);
+
+  var resultado = r + g + b;
+  //alert(resultado);
+  
+  document.getElementById("show").innerHTML = resultado;
+  
 }
 
-function rgb(r,g,b) { 
-  return "#000000";
-}
-
-/* Función que al pasar un valor RGB en representación hexadecimal
-   devuelve la representación en formato decimal con el formato RRRGGGBB
-*/
 function hex(h) {
    return "000000000";
 }
