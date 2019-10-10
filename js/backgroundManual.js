6a2a2082851c8c1ea3122f0b1768765b742d2c67
David Balboa <david@devils.local>
2019-10-10 21:32:10 +0200

    finalizado

--- js/backgroundManual.js
+++ js/backgroundManual.js
@@ -1,21 +1,82 @@
 var r = document.getElementById("R");
 var g = document.getElementById("G");
 var b = document.getElementById("B");
-document.getElementById("hex").innerHTML = "#808080";
+var h = document.getElementById("hex");
 
+/* Comprobamos la cifra introducida en Hexadecimal y modificamos */
+document.getElementById("hex").addEventListener('input', function () {
+        dividir(hex("#" + h.value));
+        if (validaColors() == true)
+            document.documentElement.style.setProperty('--colores',("#" + h.value));
+        checkNAN();
+});
+
+/* junta los 3 valores introducidos por R G B y los imprime en hex
+a la vez los manda a la variable colores */
 function join() {
-    var hexadecimal = rgb(r.value, g.value, b.value);
-
-    document.getElementById("hex").innerText = hexadecimal;
-    document.documentElement.style.setProperty('--colores', hexadecimal);
+    if (validaColors() == true)
+        var hexadecimal = rgb(r.value, g.value, b.value);
+    var text = document.getElementById('hex');
+    text.value = hexadecimal.substring(1, 7);
+    if (validaColors() == true)
+        document.documentElement.style.setProperty('--colores', hexadecimal);
 }
 
-r.oninput = function () {
+document.getElementById("R").addEventListener('input', function () {
     join();
+});
+document.getElementById("G").addEventListener('input', function () {
+    join();
+});
+document.getElementById("B").addEventListener('input', function () {
+    join();
+});
+
+/* Dividimos el valor de hexadecimal
+ para mostrarlo en los campos de RGB */
+function dividir(str){
+    r.value = str.substring(0, 3);
+    g.value = str.substring(3, 6);
+    b.value = str.substring(6, 9);
 }
-g.oninput = function () {
-    join();
+ /* Si es no hay valor, no escribes nada */
+function checkNAN(){
+    if (Number.isNaN(parseInt(r.value)))
+        r.value = "";
+    if (Number.isNaN(parseInt(g.value)))
+        g.value = "";
+    if (Number.isNaN(parseInt(b.value)))
+        b.value = "";
 }
-b.oninput = function () {
-    join();
+
+/* Valida si el numero introducido
+es solo un numero */
+function validaNum(event) {
+    if(event.charCode >= 48 && event.charCode <= 57)
+        return true;
+    return false;        
+}
+
+/* Valida si el hexadecimal introducido
+esta correctamente formateado */
+function validaHex(event) {
+    if((event.charCode >= 48 && event.charCode <= 57) 
+    || (event.charCode >= 65 && event.charCode <= 70)
+    || (event.charCode >= 97 && event.charCode <= 102))
+      return true;
+    return false;        
+}
+
+/* valida si los valores de los input son correctos,
+si lo son, cambia el color */
+function validaColors(){
+    if (Number.isNaN(parseInt(r.value)))
+        return false;
+    else if (Number.isNaN(parseInt(g.value)))
+        return false;
+    else if (Number.isNaN(parseInt(b.value)))
+        return false;
+    else if ((h.value).length == 5)
+        return false;
+    return true;
 }
--- js/rgb.js
+++ js/rgb.js
@@ -1,63 +1,32 @@
+/* ##################################################################### */
+/* #################            DECIMAL              ################### */
+/* ##################################################################### */
+
 function rgb(r, g ,b) {
-  r = parseInt(r);
-  g = parseInt(g);
-  b = parseInt(b);
-
-  r = outOfRange(r);
-  g = outOfRange(g);
-  b = outOfRange(b);
-
-  r = r.toString(16);
-  g = g.toString(16);
-  b = b.toString(16);
-
-  r = onlyOneDigit(r);
-  g = onlyOneDigit(g);
-  b = onlyOneDigit(b);
-
-    var hexa = "#" + r + g + b;
-    return (hexa.toUpperCase(hexa));
+  /* Comprobamos los valores introducidos */
+  var hexa = checkRGB(r, g, b);
+  hexa = "#" + hexa;
+  return (hexa.toUpperCase(hexa));
 }
 
-function hex(h) { 
-  //h = " " + h;
-  red = "";
-  for (i = 2; i < 4; i++){
-      red += h[i];
-  }
-  green = "";
-  for (i = 4; i < 6; i++){
-      green += h[i];
-  }
-  blue = "";
-  for (i = 6; i < 8; i++){
-      blue += h[i];
-  }
-  red = parseInt(red, 16);
-  green = parseInt(green, 16);
-  blue = parseInt(blue, 16);
-
-  red = outOfRangeHex(red);
-  green = outOfRangeHex(green);
-  blue = outOfRangeHex(blue);
-
-  red = onlyOneHex(red);
-  green = onlyOneHex(green);
-  blue = onlyOneHex(blue);
-
-  var entero = "" + red + green + blue;
-  return(entero);
+function checkRGB(r, g, b){
+  var r, g, b;
+  /* Pasamos el valor de r a un int y 
+  comprobamos que esté dentro del rango */
+  r = outOfRange(parseInt(r));
+  g = outOfRange(parseInt(g));
+  b = outOfRange(parseInt(b));
+  
+  /* Pasamos el int a hexadecimal y 
+  comprobamos si solo tiene un digito */
+  r = onlyOneDigit(r.toString(16));
+  g = onlyOneDigit(g.toString(16));
+  b = onlyOneDigit(b.toString(16));
+  return("" + r + g + b);
 }
 
-function onlyOneHex(x){
-  x = parseInt(x, 10);
-  if (x < 10)
-    x = "00" + x; 
-  else if (x < 100)
-    x = "0" + x;
-  return (x);
-}
-
+/* Comprobamos si tiene solo un digito, 
+en caso de ser así añadimos 0 */
 function onlyOneDigit(x) {
   letras = "abcdef";
   var i = 0;
@@ -71,17 +40,9 @@ function onlyOneDigit(x) {
   return(x);
 }
 
-function outOfRangeHex(x) {
-  x = parseInt(x, 10);
-  if (x > 255)
-    x = "255";
-  else if (x < 0)
-    x = "000";
-  return (x);
-}
-
+/* Comprobamos que está dentro del rango */
 function outOfRange(x) {
-  x = parseInt(x);
+  var x = parseInt(x);
   if (x > 255)
     x = "FF";
   else if (x < 0)
@@ -89,3 +50,56 @@ function outOfRange(x) {
   return (x);
 }
 
+/* ##################################################################### */
+/* #################          HEXADECIMAL            ################### */
+/* ##################################################################### */
+
+function hex(h) { 
+  /* Compruebo el hexadecimal */
+  var entero = checkHEX(h);
+  entero = "" + entero;
+  return(entero);
+}
+
+function checkHEX(h) {
+  /* Comprobamos si empieza por #
+  y si es así añadimos un ' ' */
+  if (h[0] == "#")
+    h = " " + h;
+
+  /* Divide el string en 3 variables */
+  var red = h.substring(2, 4);
+  var green = h.substring(4, 6);
+  var blue = h.substring(6, 8);
+
+  /* Convierte las variables a hexadecimal 
+  y calcula el rango y si delante tiene un
+  solo digito */
+  red = onlyOneHex(outOfRangeHex(parseInt(red, 16)));
+  green = onlyOneHex(outOfRangeHex(parseInt(green, 16)));
+  blue = onlyOneHex(outOfRangeHex(parseInt(blue, 16)));
+
+  /* Devolvemos la concatenacion de los 3 */
+  var entero = "" + red + green + blue;
+  return (entero);
+}
+
+/* Comprueba si solo tiene un digito */
+function onlyOneHex(x){
+  var x = parseInt(x, 10);
+  if (x < 10)
+    x = "00" + x; 
+  else if (x < 100)
+    x = "0" + x;
+  return (x);
+}
+
+/* Comprueba si esta fuera de rango */
+function outOfRangeHex(x) {
+  var x = parseInt(x, 10);
+  if (x > 255)
+    x = "255";
+  else if (x < 0)
+    x = "000";
+  return (x);
+}
